a878081f5ecdd8210996cb05ae8041fcc136b429
Trésor Ndayikeza <ndayikezatresor9@gmail.com>
2024-09-30 15:16:44 +0200

    uhj

--- resources/views/onBoarding.blade.php
+++ resources/views/onBoarding.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Laravel Crup App | Onboarding</title>
-      @vite('resources/css/app.css')
-</head>
+@include('includes.header')
 
 <body>
       <section class="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 bg-gray-50 dark:bg-gray-900 flex flex-col items-center justify-center min-h-screen">
@@ -18,11 +10,11 @@
 
             <div class="flex flex-col space-y-4">
                   <a href="#" class="flex items-center justify-center px-4 py-2 text-white bg-blue-600 rounded-lg shadow hover:bg-blue-700 transition duration-200">
-                        <img src="{{ asset('images/google-logo.png') }}" alt="Google Logo" class="w-5 h-5 mr-2">
+                        <i class=" fab fa-google text-xl mr-2"></i>
                         Login with Google
                   </a>
                   <a href="#" class="flex items-center justify-center px-4 py-2 text-white bg-gray-800 rounded-lg shadow hover:bg-gray-700 transition duration-200">
-                        <img src="{{ asset('images/github-logo.png') }}" alt="GitHub Logo" class="w-5 h-5 mr-2">
+                        <i  class="">
                         Login with GitHub
                   </a>
             </div>
